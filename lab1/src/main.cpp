49a7f5e1fb530bf6f097a9d5744f34c4a39c1843
Piotr Kaminski <99peterstone@gmail.com>
2022-03-15 21:43:06 +0100

    feat: algorithm 2-regret -> done

--- lab1/src/main.cpp
+++ lab1/src/main.cpp
@@ -1,10 +1,16 @@
 #include "instanceTSP.hpp"
 #include "algorithmCycleExpansion.hpp"
 #include "solution2Cycles.hpp"
+#include "algorithm2regret.hpp"
 
 #include <iostream>
+#include <cstdlib>
+#include <cstdio>
 
 int main(int argc, char ** argv){
+
+    srand(time(NULL));
+
     InstanceTSP instance;
     instance.readFromFile("../data/kroa100.tsp");
 
@@ -12,9 +18,15 @@ int main(int argc, char ** argv){
 
     AlgorithmCycleExpansion alg;
 
-    Solution2Cycles solution = alg.run(instance);
+    Solution2Cycles solution2cycles = alg.run(instance);
 
-    std::cout << solution.getTotalCost() << std::endl;
+    std::cout << solution2cycles.getTotalCost() << std::endl;
+
+    Algorithm2Regret alg2regret;
+
+    Solution2Cycles solutionRegret = alg.run(instance);
+
+    std::cout <<solutionRegret.getTotalCost() << std::endl;
 
     return 0;
 }
