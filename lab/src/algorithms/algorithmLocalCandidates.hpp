25728597c860f8e655c8186869599af20dc2d56f
Joanna Adamczyk <joannaa251@gmail.com>
2022-05-08 09:26:37 +0200

    initialized file for candidates algorithm

---
+++ lab/src/algorithms/algorithmLocalCandidates.hpp
@@ -0,0 +1,13 @@
+#ifndef ALGORITHM_LOCAL_CANDIDATES_HPP
+#define ALGORITHM_LOCAL_CANDIDATES_HPP
+
+
+#include "algorithm2cyclesMeta.hpp"
+#include "../allinstances.hpp"
+#include "../allsolutions.hpp"
+
+class AlgorithmLocalCandidates : public Algorithm2cyclesMeta{
+
+};
+
+#endif
